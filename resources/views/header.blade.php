84f64356e23fa1fec6a0f3454e3594097ae36f55
Raul Armando Paez <raul.armando,paez@gmail.com>
2019-02-11 10:32:25 -0700

    Initial commit

---
+++ resources/views/header.blade.php
@@ -0,0 +1,28 @@
+@section('header')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Visit Sinaloa!</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{asset('css/estilos.css')}}">
+</head>
+<body>
+    <header>
+        <ul class="nav justify-content-end list-menu">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Destinos Turísticos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Atractivos turístico</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Festivales</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Directorio</a>
+            </li>
+        </ul>
+    </header>
+@show
